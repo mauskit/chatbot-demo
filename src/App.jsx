006e58a48be2f3372e46515194f773baa90a3db7
Kitano Katsumi <katsumi.kawachi@gmail.com>
2021-01-18 14:23:40 +0000

    youtube ~8

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import './assets/styles/style.css';
+import {AnswersList, Chats} from './components/index'
+import defaultDataset from './dataset';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            answers: [],
+            chats: [],
+            currentId: "init",
+            dataset: defaultDataset,
+            open: false
+        }
+        this.selectAnswer = this.selectAnswer.bind(this);
+    }
+
+    displayNextQuestion = (nextQuestionId) => {
+        const chats = this.state.chats;
+        chats.push({
+            text: this.state.dataset[nextQuestionId].question,
+            type: 'question'
+        })
+        this.setState({
+            answers: this.state.dataset[nextQuestionId].answers,
+            chats: chats,
+            currentId: nextQuestionId
+        })
+    }
+
+    selectAnswer =  (selectedAnswer, nextQuestionId) => {
+        switch(true){
+            case(nextQuestionId === 'init'):
+                this.displayNextQuestion(nextQuestionId);
+                break;
+
+            case(/^https:*/.test(nextQuestionId)):
+                const a = document.createElement('a');
+                a.href = nextQuestionId;
+                a.target = '_brank';
+                a.click();
+                break;
+            
+            default:
+                const chats = this.state.chats;
+                chats.push({
+                    text: selectedAnswer,
+                    type: 'answer'
+                });
+
+                this.setState({
+                    chats: chats,
+                    currentId: nextQuestionId
+                });
+
+                setTimeout(() => (this.displayNextQuestion(nextQuestionId)),500);
+                break;
+        }
+    }
+
+    componentDidMount(){
+        const initAnswer = "";
+        this.selectAnswer(initAnswer, this.state.currentId);
+    }
+
+    componentDidUpdate(){
+        const scrollArea = document.getElementById("scroll-area")
+        if(scrollArea){
+            scrollArea.scrollTop = scrollArea.scrollHeight;
+        }
+    }
+
+    render() {
+        return(
+            <div className="c-section">
+                <div className="c-box">
+                    <Chats chats={this.state.chats}/>
+                    <AnswersList
+                        answers={this.state.answers}
+                        select={this.selectAnswer}
+                    />
+                </div>
+            </div> 
+        );
+    }
+}
+
